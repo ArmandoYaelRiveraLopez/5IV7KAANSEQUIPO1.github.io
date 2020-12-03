8bca34a8e5a5a58814a36c61c15f3cb19b52cc9b
Armando Yael Rivera Lopez <>
2020-12-02 18:09:48 -0600

    inicio de sesión

---
+++ JS/popupJS.js
@@ -0,0 +1,13 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("signin").addEventListener('click',
+  function(){
+
+    $('.modal').fadeIn('slow');
+    document.querySelector('.modal').style.display = 'flex';
+  });
+
+});
