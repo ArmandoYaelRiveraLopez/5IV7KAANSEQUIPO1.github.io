64b75b4d98e6c085b21a282f7680c59ef2ab36c7
Armando Yael Rivera Lopez <>
2020-12-03 22:30:16 -0600

    registro

--- JS/popupJS.js
+++ JS/popupJS.js
@@ -3,11 +3,46 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+
   document.getElementById("signin").addEventListener('click',
   function(){
 
-    $('.modal').fadeIn('slow');
-    document.querySelector('.modal').style.display = 'flex';
+    $('.modal-sign-in').fadeIn('slow');
+    document.querySelector('.modal-sign-in').style.display = 'flex';
+
+  });
+
+  document.getElementById("close").addEventListener('click',
+  function(){
+
+    $('.modal-sign-in').fadeOut('slow');
+
+  });
+
+  document.getElementById("acount-1").addEventListener('click',
+  function(){
+
+    $('.modal-sign-in').fadeOut('slow');
+    $('.modal-sign-up').fadeIn('slow');
+    document.querySelector('.modal-sign-up').style.display = 'flex';
+
+  });
+
+  document.getElementById("acount-2").addEventListener('click',
+  function(){
+
+
+    $('.modal-sign-up').fadeIn('slow');
+    document.querySelector('.modal-sign-up').style.display = 'flex';
+
+  });
+
+  document.getElementById("close-2").addEventListener('click',
+  function(){
+
+    $('.modal-sign-up').fadeOut('slow');
+
+
   });
 
 });
